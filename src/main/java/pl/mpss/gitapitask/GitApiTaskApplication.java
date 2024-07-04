7a3ab9407925fad8f4a50dac426190e3bbf5c94a
mateuszparasiewicz <m.parasiewicz00@gmail.com>
2024-07-04 11:18:46 +0200

    Initial commit

---
+++ src/main/java/pl/mpss/gitapitask/GitApiTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.mpss.gitapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitApiTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mpss/gitapitask/GitApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mpss.gitapitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
